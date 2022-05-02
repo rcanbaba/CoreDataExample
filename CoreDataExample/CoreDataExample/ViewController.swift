fb2eb4c5b11232d69f14928ff4961131081cdaf7
Can Babaoğlu <canbabaoglu@scorpapp.com>
2022-05-02 12:06:40 +0300

    Part1: main project flow completed, storyboard and UI elements integrated.

---
+++ CoreDataExample/CoreDataExample/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Can Babaoğlu on 2.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "sampleCore"
+        
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+    
+    
+}
